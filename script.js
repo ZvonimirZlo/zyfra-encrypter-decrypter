0d60893e411d0f05ae12b3980c19b2cafad3b955
ZvonimirZlo <lovrez2@gmail.com>
2026-08-18 17:33:08 +0200

    Added matrix rain background

--- script.js
+++ script.js
@@ -25,7 +25,47 @@ const pasteInBoxForDecryption = document.getElementById('paste_text_in_encryptio
 const deleteTextFromBoxForDecryption = document.getElementById('delete_from_area_for_decryption');
 const copyFromAreaForDecryption = document.getElementById('copy_text_from_area_for_decryption');
 const deleteTextFromDecrypted = document.getElementById('delete_text_from_decrypted');
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = latin + nums;
+const rainDrops = [];
+const fontSize = 16
+const columns = canvas.width/fontSize
+
+for( let x = 0; x < columns; x++ ) {
+    rainDrops[x] = 1;
+}
+const draw = () => {
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.fillStyle = '#0F0';
+	context.font = fontSize + 'px monospace';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+};
+setInterval(draw, 30);
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 //**ENCRYPTION TEXTAREA TEXT HANDLERS
 
 //Paste text in encryption text box handler
